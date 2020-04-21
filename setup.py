40f62e5a561b82d075cd8b17cd3d0f6419186c4b
Vikram Voleti <vikky2904@gmail.com>
2020-04-21 19:46:59 -0400

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(name='sls',
+      version='0.1.0',
+      description='Stochastic Line Search',
+      url='git@github.com:IssamLaradji/sls.git',
+      maintainer='Issam Laradji',
+      maintainer_email='issam.laradji@gmail.com',
+      license='MIT',
+      packages=['sls'],
+      zip_safe=False,
+      install_requires=[
+        'tqdm>=0.0'
+        'numpy>=0.0',
+        'pandas>=0.0',
+        'Pillow>=0.0',
+        'scikit-image>=0.0',
+        'scikit-learn>=0.0',
+        'scipy>=0.0',
+        'scikit-learn>=0.0',
+        'torch>=0.0',
+        'torchvision>=0.0',
+      ]),
